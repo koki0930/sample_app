274c0e0b871c942c95988b4a7cc7587ba0e3cc4e
koki0930 <kokisan0930@gmail.com>
2021-03-12 12:21:19 +0000

    Implement advanced login

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,3 @@
-const webpack = require('webpack')
+const { environment } = require('@rails/webpacker')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+module.exports = environment
